77f7e6acdbae0365338d71ba18e4667dab0bb1ec
Pranvera97 <pranveragerbeshi@gmail.com>
2021-12-09 11:59:19 +0100

    Skapa konto (snart färdigt)

--- ADMIN/sign-up.php
+++ ADMIN/sign-up.php
@@ -1,31 +1,70 @@
 <?php
 require_once "../INCLUDES/header.php";
-?>
 
+function allUsers(){
+    $json = file_get_contents("../API/users.json");
+    $data = json_decode($json, true);
+    $allUsers = $data;
+
+    return $allUsers;
+}
+
+function addUser($postInfo){
+    //nycklar för den nya hunden
+    $newUser = [
+        "username" => $postInfo["username"],
+        "email" => $postInfo["email"],
+        "password" => $postInfo["password"],  
+        "pictures" => "welcome.img",
+        "color-scheme" => 0  
+    ];
+    //foreachen räknar ut den nya hundens id utifrån vilka som redan finns
+    $highestID = 0;
+
+    $allUsers = allUsers();
+    foreach($allUsers as $user){
+        if ($user["id"] > $highestID){
+            $highestID = $user["id"];
+        }
+    }
+
+    //ID:et av den nya hunden
+    $newUser["id"] = $highestID + 1;
+    //lägg till hund i db.json
+    $data = json_decode(file_get_contents("../API/users.json"), true);
+    array_push($data, $newUser);
+    $json = json_encode($data, JSON_PRETTY_PRINT);
+    file_put_contents("../API/users.json", $json);
+}
+
+//Kollar vad som är skrivet i våra inputs och skapar variabler med dess info
+if (isset($_POST["username"], $_POST["email"], $_POST["password"])) {
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    //Om inputsen är tomma skapas inte en ny user och ett error visas
+    if ($username == "" || $email == "" || $password == "") {
+        $error = 1;
+    } else {
+        addUser($_POST);
+    }
+}
+
+?>
 
 <form method="POST" action="sign-up.php">
 <div id="signInForm">
     <h1>Create an account</h1>
-    <input type="text" name="name" placeholder="Name">
+    <input type="text" name="username" placeholder="Username">
     <input type="text" name="email" placeholder="Email">
     <input type="password" name="password" placeholder="Password">
     <input type="password" name="password" placeholder="Confirm password">
     <button id="sign-up-button">Sign in</button>
     </div>
+    <?php
+//Error eller info om tilllagd hund
+    if (isset($error)) { ?>
+        <p class="error">Write something!</p>
+    <?php } ?>
 </form>
-
-<?php
-if (isset($_GET["error"])){
-    $error = $_GET["error"];
-
-    if ($error == 1) { //Fälten är tomma
-        echo '<p class="error">Please fill out all fields.</p>';
-    } elseif ($error == 2) { //fel lösenord
-        echo '<p class="error">Incorrect password.</p>';
-    } elseif ($error == 3) { //fel email
-        echo '<p class="error">The email does not exist.</p>';
-    } elseif ($error == 4) { //fel kombination av lösenord och
-        echo '<p class="error">Incorrect username or password.</p>';
-    }
-}
-?>
